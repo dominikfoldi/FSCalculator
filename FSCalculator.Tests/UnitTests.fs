9cf2e1b8d1421487aae819ae6c05dd66bf4d40e2
Dominik Foldi <dominik.foldi@outlook.com>
2016-11-18 11:49:18 +0100

    Created experiment core types

--- FSCalculator.Tests/UnitTests.fs
+++ FSCalculator.Tests/UnitTests.fs
@@ -1,5 +1,13 @@
 ﻿namespace FSCalculator.Tests
 
 open Xunit
+open FSCalculator
+
+module CalculatorTest =
+
+    [<Fact>]
+    let ``When nothing is entered expect 0``() =
+        let a = FSCalculator.Negate
+
 
 
--- FSCalculator/Program.fs
+++ FSCalculator/Program.fs
@@ -1,4 +1,25 @@
-﻿[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+﻿namespace FSCalculator 
+
+type Operation = 
+    | Add
+    | Subtract
+    | Multiply
+    | Divide
+    | Negate
+    | Result
+
+type Expression = 
+    private
+    | Operation of Operation
+    | Number of int
+    | Delete
+    | Clear
+
+type State = { History: string; Result: float; Operation: Operation }
+
+module Calculator =
+
+    [<EntryPoint>]
+    let main argv = 
+        let d = Delete
+        0 // return an integer exit code
