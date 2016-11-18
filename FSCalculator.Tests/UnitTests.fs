90faea8cc754e44b4af5db22009f829cb5560d93
Dominik Foldi <dominik.foldi@outlook.com>
2016-11-18 15:12:32 +0100

    Implemented negation and decimal point processing

--- FSCalculator.Tests/UnitTests.fs
+++ FSCalculator.Tests/UnitTests.fs
@@ -27,9 +27,48 @@ module CalculatorTest =
         Assert.Equal("12", result.Input)
 
     [<Fact>]
-    let ``Not empty InputState modified by one number should concat that number``() =
+    let ``Decimal point should be added after a number``() =
         let inputState = InputState.Empty
 
-        let result = ModifyInputState (Number 1) inputState |> ModifyInputState (Number 2)
+        let result = ModifyInputState (Number 1) inputState |> ModifyInputState DecimalPoint
 
-        Assert.Equal("12", result.Input)
+        Assert.Equal("1.", result.Input)
+
+    [<Fact>]
+    let ``Number should added after decimal point``() =
+        let inputState = InputState.Empty
+
+        let result = ModifyInputState DecimalPoint inputState |> ModifyInputState (Number 1)   
+
+        Assert.Equal("0.1", result.Input)
+
+    [<Fact>]
+    let ``Only one decimal point should be added``() =
+        let inputState = InputState.Empty
+
+        Assert.Throws<MoreThanOneDecimalPointException>(fun () -> 
+            ModifyInputState DecimalPoint inputState |> ModifyInputState DecimalPoint |> ignore )
+
+    [<Fact>]
+    let ``Maximum 16 number should be added``() =
+        let inputState = InputState.Empty
+
+        Assert.Throws<MoreThan16NumberException>(fun () -> 
+            Seq.replicate 17 (Number 4) |> Seq.fold (fun s i -> ModifyInputState i s) inputState
+            |> ignore)
+
+    [<Fact>]
+    let ``Negation should put a minus sign to the front``() =
+        let inputState = InputState.Empty
+
+        let result = ModifyInputState (Number 2) inputState |> ModifyInputState Negate 
+
+        Assert.Equal("-2", result.Input)
+
+    [<Fact>]
+    let ``Double negation should clear the minus sign from the front``() =
+        let inputState = InputState.Empty
+
+        let result = ModifyInputState (Number 2) inputState |> ModifyInputState Negate |> ModifyInputState Negate
+
+        Assert.Equal("2", result.Input)
--- FSCalculator/Models.fs
+++ FSCalculator/Models.fs
@@ -11,17 +11,20 @@ module Models =
     type Input = 
         | Operation of Operation
         | Number of int
-        | DecimalPoint of char
+        | DecimalPoint
         | Negate
         | Result
         | Delete
         | Clear
 
     type InputState = 
-        { Input: string; DecimalPoint: bool }
+        { Input: string; DecimalPoint: bool; NumberCount: int; Negated: bool }
 
         static member Empty = 
-            { Input = "0"; DecimalPoint = false }
+            { Input = "0"; DecimalPoint = false; NumberCount = 1; Negated = false }
+
+    exception MoreThanOneDecimalPointException
+    exception MoreThan16NumberException
 
     let ModifyInputState (input: Input) (inputState: InputState) = 
         match input with
@@ -29,6 +32,27 @@ module Models =
                 if inputState.Input = "0" then 
                     { inputState with Input = n.ToString() }
                 else
-                    { inputState with Input = inputState.Input + n.ToString() }
+                    if inputState.NumberCount < 16 then
+                        { inputState with 
+                            Input = inputState.Input + n.ToString()
+                            NumberCount = inputState.NumberCount + 1 }
+                    else
+                        raise MoreThan16NumberException
+            | DecimalPoint -> 
+                if not inputState.DecimalPoint then
+                    { inputState with 
+                        Input = inputState.Input + "."
+                        DecimalPoint = true }
+                else
+                    raise MoreThanOneDecimalPointException
+            | Negate ->
+                if not inputState.Negated then
+                    { inputState with 
+                        Input = "-" + inputState.Input
+                        Negated = true }
+                else
+                    { inputState with 
+                        Input = inputState.Input.Substring 1
+                        Negated = false }
 
     type ProcessedState = { History: string; Result: float; PendingOperation: Operation; PendingNumber: float }
