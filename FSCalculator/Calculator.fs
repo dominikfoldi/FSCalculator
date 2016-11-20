0320c5741a98aa50035625f698a98857c1180ccd
Dominik Foldi <dominik.foldi@outlook.com>
2016-11-20 17:59:50 +0100

    Fixing issue when inputstate doesn't reset between EvaluateInput and NumberInput

--- FSCalculator/Calculator.fs
+++ FSCalculator/Calculator.fs
@@ -13,13 +13,19 @@ module Calculator =
     let mutable inputState = InputState.Empty
     let mutable calculatorState = CalculatorState.Empty
 
+    let mutable evaluated = false
+
     let GetInput (input: CalculatorInput) =
         match input with
         | NumberInput numberInput ->
+            if evaluated then
+                inputState <- InputState.Empty
+                evaluated <- false
             inputState <- ModifyInputState numberInput inputState
         | EvaluateInput evaluateInput ->
             calculatorState <- ModifyCalculatorState evaluateInput (float inputState.Input) calculatorState
             inputState <- { inputState with Input = (string calculatorState.Result) }
+            evaluated <- true
         | Clear ->
             inputState <- InputState.Empty
             calculatorState <- CalculatorState.Empty
